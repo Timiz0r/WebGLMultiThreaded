f331b8a985f845a4c7dab69b3551a69d55750802
Timiz0r <5377118+Timiz0r@users.noreply.github.com>
2025-03-21 02:32:43 -0700

    Clarify other ways to handle game logic events unity-side

--- Assets/platform_webgl/WebGLGameLogic.cs
+++ Assets/platform_webgl/WebGLGameLogic.cs
@@ -10,17 +10,17 @@ public class WebGLGameLogic : MonoBehaviour
     [DllImport("__Internal")]
     private static extern void GameLogicInterop_Initialize();
 
-    // NOTE: it's also hypothetically possible to pump the "request id" to unity (returned here, passed to callbacks),
-    // then hook them up to async-style stuff like coroutines or async-await
     [DllImport("__Internal")]
     private static extern void GameLogicInterop_Update(float time);
 
     [DllImport("__Internal")]
     private static extern void GameLogicInterop_RegisterEventListener(string gameObjectName);
 
-    // TODO: instead of driving everything from one component, allow, for instance, counter to get state itself
-    // this component simply does the initial legwork
-    // this work will also include rearranging the scene objects more sanely. 
+    // in this example, this one component is the central place handling events and manipulating the scene.
+    // one alternative is adding and invoking unity events from here
+    // another alternative is having "intermediate" .net events
+    //   for instance, first add `public static event`s here.
+    //   then, other components can listen to them as they wish.
     private void StateChanged(string json)
     {
         UntypedStateChange untypedStateChange = (UntypedStateChange)JsonUtility.FromJson(json, typeof(UntypedStateChange));
@@ -46,19 +46,6 @@ public class WebGLGameLogic : MonoBehaviour
                 Debug.LogError($"Unknown state change: {json}");
                 break;
         }
-
-
-    }
-
-    private void MessageChanged(string message)
-    {
-        var obj = transform.Find("Message");
-        obj.GetComponent<TextMeshPro>().text = message;
-    }
-
-    private static void Error(string error)
-    {
-        Debug.LogError($"Failed to update game logic.: {error}");
     }
 
     void Start()
