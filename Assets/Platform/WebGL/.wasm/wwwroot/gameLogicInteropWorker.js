b576a7923660ad05c0ac484a1ddc951721f80f93
Timiz0r <5377118+Timiz0r@users.noreply.github.com>
2025-04-06 16:53:21 -0700

    Don't need Proxy for registering game logic worker subscriber
    Was having trouble getting data flowing and tried it as a fix.
    Now that everything is working, it looks like we can safely ditch the proxy.

--- Assets/Platform/WebGL/.wasm/wwwroot/gameLogicInteropWorker.js
+++ Assets/Platform/WebGL/.wasm/wwwroot/gameLogicInteropWorker.js
@@ -5,24 +5,7 @@ const { setModuleImports, getAssemblyExports, getConfig } = await dotnet.create(
 
 const config = getConfig();
 const assemblyExports = await getAssemblyExports(config.mainAssemblyName);
-
-const subscriberHandler = new class {
-    set(obj, prop, value) {
-        if (prop === "subscriber") {
-            this.subscriber = value;
-        } else {
-            obj[prop] = value;
-        }
-        return true;
-    }
-    get(obj, prop) {
-        if (prop === "subscriber") {
-            return this.subscriber;
-        }
-        return obj[prop];
-    }
-}();
-const interop = new Proxy(assemblyExports.GameLogicInterop, subscriberHandler);
+const interop = assemblyExports.GameLogicInterop;
 
 setModuleImports("GameLogic", {
     StateChanged: data => { interop.subscriber && interop.subscriber(data); }
